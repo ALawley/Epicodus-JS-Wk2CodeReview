0406b8176b1f167ca257db8499b18bb3dc217b96
Anna Lawley <alawley1@gmail.com>
2016-03-27 14:44:01 -0700

    add day constructor and app component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,24 @@
 import { Component } from 'angular2/core';
+import { Day } from './day.model';
 
 @Component({
   selector: 'my-app',
   template: `
     <div class="container">
-      <h1>Skeleton Angular2 App!</h1>
-    </div>  
+      >day-list [dayList]="days"></day-list>
+    </div>
   `
 })
 
 export class AppComponent {
-
+  public days: Day[];
+  constructor() {
+    new Day("Monday")
+    new Day("Tuesday")
+    new Day("Wednesday")
+    new Day("Thursday")
+    new Day("Friday")
+    new Day("Saturday")
+    new Day("Sunday")
+  }
 }
