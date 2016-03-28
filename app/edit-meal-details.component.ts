f09205643c5bda618c398b92c31ab71e35dd1d89
Anna Lawley <alawley1@gmail.com>
2016-03-27 21:59:16 -0700

    fix day editing of meals

--- app/edit-meal-details.component.ts
+++ app/edit-meal-details.component.ts
@@ -1,14 +1,17 @@
 import { Component, EventEmitter } from 'angular2/core';
 import { Meal } from './meal.model';
+import { DaysSelectComponent} from './days-select.component';
 
 @Component({
   selector: 'edit-meal-details',
   inputs: ['meal'],
   outputs: ['onEndEdit'],
+  directives: [DaysSelectComponent],
   template: `
     <div class="meal-form">
       <h4>Edit Meal Information</h4>
       <input [(ngModel)]="meal.name" class="input-md">
+      <days-select [meal]="meal" (onDaySelect)="setDay($event)"></days-select>
       <input [(ngModel)]="meal.calories" type="number" class="input-md">
       <input [(ngModel)]="meal.details" class="input-md">
       <button (click)=endEdit() class="btn-success btn-md add-button">Done Editing</button>
@@ -28,5 +31,8 @@ export class EditMealDetailsComponent {
   deleteMeal() {
     this.meal.delete = true;
     this.onEndEdit.emit(this.meal);
+  }
+  setDay(dayValue: string) {
+    this.meal.day = parseInt(dayValue);
   }
 }
