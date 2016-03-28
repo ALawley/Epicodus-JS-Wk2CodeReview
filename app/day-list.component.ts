0c530d5528c6adc9c37377591671678522d08d34
Anna Lawley <alawley1@gmail.com>
2016-03-27 21:48:57 -0700

    build form to add new meals

--- app/day-list.component.ts
+++ app/day-list.component.ts
@@ -4,13 +4,15 @@ import { Day } from './day.model';
 import { DayPipe } from './day.pipe';
 import { DeletePipe } from './delete.pipe';
 import { DayDisplayComponent } from './day.component';
+import { NewMealComponent } from './new-meal.component';
 
 @Component({
   selector: 'day-list',
   inputs: ['dayList', 'mealList'],
   pipes: [DayPipe, DeletePipe],
-  directives: [DayDisplayComponent],
+  directives: [DayDisplayComponent, NewMealComponent],
   template: `
+    <new-meal (onSubmitNewMeal)="createMeal($event)"></new-meal>
     <day-display *ngFor="#currentDay of dayList" [day]="currentDay" [mealList]="mealList | delete | daypipe:currentDay.id"></day-display>
   `
 })
@@ -18,4 +20,11 @@ import { DayDisplayComponent } from './day.component';
 export class DayListComponent {
   public dayList: Day[];
   public mealList: Meal[];
+  createMeal(userInputArray: any[]): void {
+    console.log(userInputArray);
+    this.mealList.push(
+      new Meal(userInputArray[0], userInputArray[1], userInputArray[2], userInputArray[3])
+    );
+    console.log(this.mealList);
+  }
 }
---
+++ app/new-meal.component.ts
@@ -0,0 +1,41 @@
+import { Component, EventEmitter } from 'angular2/core';
+import { Meal } from './meal.model';
+import { DaysSelectComponent} from './days-select.component';
+
+@Component({
+  selector: 'new-meal',
+  outputs: ['onSubmitNewMeal'],
+  directives: [DaysSelectComponent],
+  template: `
+    <div class="meal-form">
+      <h4>Add a Meal:</h4>
+      <form (submit)="addMeal(newName, newCalories, newDetails)">
+        <input required placeholder="name" class="input-lg" #newName>
+        <days-select (onDaySelect)="setDay($event)"></days-select>
+        <input required placeholder="calories" type="number" class="input-lg" #newCalories>
+        <input required placeholder="details" class="input-lg" #newDetails>
+        <button type="submit" class="btn-success btn-lg add-button">Add</button>
+      </form>
+    </div>
+  `
+})
+
+export class NewMealComponent {
+  public newMealDay: number = 0;
+  public onSubmitNewMeal: EventEmitter<any[]>;
+  constructor() {
+    this.onSubmitNewMeal = new EventEmitter();
+  }
+  addMeal(newName: HTMLInputElement, newCalories: HTMLInputElement, newDetails: HTMLInputElement){
+    var newMealInputs: any[] = [];
+    newMealInputs.push(newName.value);
+    newMealInputs.push(parseInt(newCalories.value));
+    newMealInputs.push(newDetails.value);
+    newMealInputs.push(this.newMealDay);
+    console.log(newMealInputs);
+    this.onSubmitNewMeal.emit(newMealInputs);
+  }
+  setDay(dayValue: string) {
+    this.newMealDay = parseInt(dayValue);
+  }
+}
