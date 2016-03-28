f2e1d7a370ac205ddeb959e7546b38a486164ef1
Anna Lawley <alawley1@gmail.com>
2016-03-27 18:33:36 -0700

    add form to edit meals

---
+++ app/edit-meal-details.component.ts
@@ -0,0 +1,35 @@
+import { Component, EventEmitter } from 'angular2/core';
+import { Meal } from './meal.model';
+
+@Component({
+  selector: 'edit-meal-details',
+  inputs: ['meal'],
+  outputs: ['onEndEdit'],
+  template: `
+    <div class="meal-form">
+      <h4>Edit Meal Information</h4>
+      <input [(ngModel)]="meal.name" class="input-md">
+      <days-select [meal]="meal" (onDaySelect)="updateDay($event)"></days-select>
+      <input [(ngModel)]="meal.details" class="input-md">
+      <button (click)=endEdit() class="btn-success btn-md add-button">Done Editing</button>
+      <button (click)=deleteMeal() class="btn-success btn-md add-button">Delete Meal</button>
+    </div>
+  `
+})
+export class EditMealDetailsComponent {
+  public meal: Meal;
+  public onEndEdit: EventEmitter<Meal>;
+  constructor() {
+    this.onEndEdit = new EventEmitter();
+  }
+  endEdit() {
+    this.onEndEdit.emit(this.meal);
+  }
+  deleteMeal() {
+    this.meal.delete = true;
+    this.onEndEdit.emit(this.meal);
+  }
+  updateDay(dayValue: string) {
+    this.meal.day = parseInt(dayValue);
+  }
+}
